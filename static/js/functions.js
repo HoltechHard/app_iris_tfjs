d80876c68f2ec2d76237969323cebf1cd0153114
Holger <hadronx@live.com>
2023-07-22 23:16:51 +0300

    static files

---
+++ static/js/functions.js
@@ -0,0 +1,37 @@
+$('#btnTrain').click(function(){
+    $('#btnTrain').prop('disabled', true);
+    $('#btnTrain').empty().append('Training ...');
+
+    $.ajax({
+        type: 'POST',
+        url: '/train',
+        success: function(result){
+            console.log(result);
+            $('#btnSend').prop('disabled', false);
+            $('#btnTrain').empty().append('Trained');            
+        }
+    });
+});
+
+$('#btnSend').click(function(){
+    var sepalLength = $('#sepalLength').val();
+    var sepalWidth = $('#sepalWidth').val();
+    var petalLength = $('#petalLength').val();
+    var petalWidth = $('#petalWidth').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/predict',
+        data: {
+            sepalLength: sepalLength,
+            sepalWidth: sepalWidth,
+            petalLength: petalLength,
+            petalWidth: petalWidth
+        },
+        success: function(result){
+            console.log(result);
+            $('#result').empty().append(result.class_pred);
+            $('#prob').empty().append(result.prob_pred.toFixed(4) + '%');
+        }
+    });
+});
